4ca239240ebe70d8648353fd533f8b3eaa1adddb
Aditya Kalra <aditya.kalra.cs@gmail.com>
2025-07-02 00:18:51 +0530

    Practiced Login and SignUp via React Component Rendering

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { LoginPage } from './Components/LoginPage';
+import { SignUpPage } from './Components/SignUpPage';
+
+
+function App() {
+
+ const[isLoggedIn,setLoggedIn] = useState(true);
+
+ function changeState(){
+      isLoggedIn == true ? setLoggedIn(false) : setLoggedIn(true);
+ }
+
+  if(isLoggedIn){
+    
+    return (
+      <div className='relative'>
+        <LoginPage></LoginPage> 
+        <button
+        onClick={changeState}
+            type="submit"
+            className='z-50 fixed right-4 top-4 sm:right-4 sm:top-4 
+             text-xs sm:text-sm md:text-base 
+             px-3 sm:px-4 md:px-5 py-2 sm:py-2.5 
+             bg-gradient-to-r from-violet-600 to-indigo-700 
+             text-white font-semibold rounded-xl 
+             shadow-md hover:scale-105 transition-transform'
+>
+            Change State
+          </button>
+      </div>
+    
+  )
+   
+  }
+  else{
+    return (
+      <div className='relative'>
+        <SignUpPage></SignUpPage> 
+        <button
+        onClick={changeState}
+            type="submit"
+          className='z-50 fixed right-4 top-4 sm:right-4 sm:top-4 
+             text-xs sm:text-sm md:text-base 
+             px-3 sm:px-4 md:px-5 py-2 sm:py-2.5 
+             bg-gradient-to-r from-violet-600 to-indigo-700 
+             text-white font-semibold rounded-xl 
+             shadow-md hover:scale-105 transition-transform'
+>
+            Change State
+          </button>
+      </div>
+    
+  )
+  }
+}
+
+export default App
---
+++ src/Components/LoginPage.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+export const LoginPage = () => {
+  const [userName, setUserName] = useState('');
+  const[userPassword,setUserPassword] = useState('');
+  const[success,setSuccess] = useState(null);
+    
+  const handleUserName = (e) => {
+    setUserName(e.target.value);
+  };
+
+ function handleUserPassword(e){
+    setUserPassword(e.target.value);
+  }
+
+
+ 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitting...");
+    console.log("Username:", userName);
+     if (
+      userName.trim() !== "" && userPassword.trim() !== ""
+    ) {
+      setSuccess(true);
+    } else {
+      setSuccess(false);
+    }
+    
+    
+  };
+
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-gradient-to-br from-gray-900 to-black p-6">
+      <div className="bg-white/10 backdrop-blur-lg p-8 rounded-2xl shadow-xl w-full max-w-md">
+        <h1 className="text-3xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-violet-500 to-indigo-500 mb-6">
+          Login Here
+        </h1>
+
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <input
+            type="text"
+            onChange={handleUserName}
+            value={userName}
+            placeholder="Enter your username"
+            className="w-full px-4 py-3 rounded-lg border border-violet-400 bg-white/80 placeholder-gray-700 focus:outline-none focus:ring-2 focus:ring-violet-500"
+          />
+          <input
+            type="password"
+            onChange={handleUserPassword}
+            value={userPassword}
+            placeholder="Enter your Password"
+            className="w-full px-4 py-3 rounded-lg border border-violet-400 bg-white/80 placeholder-gray-700 focus:outline-none focus:ring-2 focus:ring-violet-500"
+          />
+          <button 
+            type="submit"
+            className="cursor-pointer w-1/2 mx-auto mt-4 bg-gradient-to-r from-violet-600 to-indigo-700 text-white font-semibold py-3 rounded-xl shadow-md hover:scale-105 transition-transform"
+          >
+            Submit
+          </button>
+
+       {success !== null && (
+  <div className={`text-center text-white font-semibold p-4 rounded-xl ${success ? 'bg-green-500' : 'bg-red-500'}`}>
+    {success ? "Login Successful!" : "Login Failed"}
+  </div>
+)}
+
+
+        </form>
+      </div>
+    </div>
+  );
+};
